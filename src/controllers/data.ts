7cef995204968a2a1463f8a21abb785338ef86d7
OZ <zybrukzybik997@gmail.com>
2021-03-01 20:25:04 +0200

    add isHaveExpressionsArr and replace || with &&

--- src/controllers/data.ts
+++ src/controllers/data.ts
@@ -2,7 +2,7 @@ import assert from 'assert'
 
 import {Context} from "koa";
 
-import {isArr, isEmptyArr, isObj, isEmptyObj, isArrWithOneEmptyEl} from '../utils/validation'
+import {isArr, isEmptyArr, isObj, isEmptyObj, isHaveExpressionsArr, isArrWithOneEmptyEl} from '../utils/validation'
 import writeResults from '../utils/writeResult'
 import {responseOk} from '../utils/responses'
 import parser from '../utils/parser'
@@ -14,11 +14,11 @@ interface Body {
 async function data(ctx: Context) {
     const body: Body = ctx.request.body
 
-    assert(isObj(body) || !isEmptyObj(body), 'invalid')   //  check obj || empty obj
+    assert(isObj(body) && !isEmptyObj(body) && isHaveExpressionsArr(body), 'invalid body')
 
     const expr: Array<string> = body.expressions
 
-    assert(isArr(expr) && !isEmptyArr(expr) && !isArrWithOneEmptyEl(expr), 'invalid')     //  check arr
+    assert(isArr(expr) && !isEmptyArr(expr) && !isArrWithOneEmptyEl(expr), 'invalid array')
 
     const results: Array<string> = parser(expr)
 
