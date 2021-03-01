c6e7e2764d4218d3c82d36eb12377aa7f8a6a134
OZ <zybrukzybik997@gmail.com>
2021-03-01 20:25:36 +0200

    rename context to ctx fix ok to results in ctx.body

--- src/utils/responses.ts
+++ src/utils/responses.ts
@@ -1,18 +1,18 @@
 import readResults from './readResult'
 import {Context} from 'koa'
 
-function responseInvalid(error: Error, context: Context): void {
+function responseInvalid(error: Error, ctx: Context): void {
     console.log(error)
 
-    context.status = 400
-    context.body = 'invalid'
+    ctx.status = 400
+    ctx.body = 'invalid'
 }
 
-async function responseOk(context: Context): Promise<void> {
+async function responseOk(ctx: Context): Promise<void> {
     const results: Array<string> = await readResults()
 
-    context.status = 200
-    context.body = {ok: results}
+    ctx.status = 200
+    ctx.body = {results: results}
 }
 
 export {
