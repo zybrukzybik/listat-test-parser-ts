377c8e41a11dca00d8343b0906215c1c98dc7dba
OZ <zybrukzybik997@gmail.com>
2021-03-01 20:12:44 +0200

    add errors.ts for checking errors in global errors handler

---
+++ src/utils/errors.ts
@@ -0,0 +1,11 @@
+const errors: Array<string> = [
+    'invalid symbols',
+    'invalid start symbol',
+    'invalid parentheses',
+    'invalid double operators',
+    'invalid order of operators',
+    'invalid body',
+    'invalid array'
+]
+
+export default errors
