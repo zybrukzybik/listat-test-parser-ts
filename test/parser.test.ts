d4b682952dca7ec896ea1dd5c6b8f673b19dc808
OZ <zybrukzybik997@gmail.com>
2021-03-01 19:22:50 +0200

    add more tests

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,4 +1,4 @@
-import assert from 'assert'
+import assert = require('assert')
 import {describe, it} from 'mocha'
 
 import parser from '../src/utils/parser'
@@ -11,13 +11,11 @@ const validData = [
     "(-10*10/(-10)+10)*(-100/(-10))",   //  200
 ]
 
-const validDataExpected = [
-    25,
-    100,
-    40,
-    2,
-    200
-]
+const validDataExpected = ['25', '100', '40', '2', '200']
+
+const validDataOnePositiveDigit = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+const validDataOneNegativeDigit = ['-1', '-2', '-3', '-4', '-5', '-6', '-7', '-8', '-9', '-0']
 
 const notValidSymbols = [
     'a,.\\|&^%$#@!`"\''
@@ -35,11 +33,35 @@ const twoMoreDiffOp = [
     '100*10-+9'
 ]
 
+const startWithWrongSymbols = [
+    '+100+100'
+]
+
+const oneWrongOperator = [
+    '*'
+]
+
+const oneCorrectOperator = [
+    '-'
+]
+
+const operatorsOnly = [
+    '-+-*/'
+]
+
 describe('All', () => {
     describe('Valid symbols', () => {
-        it('should calculate', () => {
+        it('should calculate whole array', () => {
             assert.deepStrictEqual(parser(validData), validDataExpected)
         })
+
+        it('should calculate if have only one positive digit', () => {
+            assert.deepStrictEqual(parser(validDataOnePositiveDigit), validDataOnePositiveDigit)
+        });
+
+        it('should calculate if have only one negative digit', () => {
+            assert.deepStrictEqual(parser(validDataOneNegativeDigit), validDataOneNegativeDigit)
+        });
     })
 
     describe('Invalid symbols', () => {
@@ -58,6 +80,23 @@ describe('All', () => {
         it('should rejects if have 2+ different operators together', () => {
             return assert.throws(() => parser(twoMoreDiffOp), /invalid/)
         });
+
+        it('should rejects if not start with digit or "-"', () => {
+            return assert.throws(() => parser(startWithWrongSymbols), /invalid/)
+        });
+
+        it('should rejects if have operators only', () => {
+            return assert.throws(() => parser(operatorsOnly), /invalid/)
+        });
+
+        it('should rejects if have one wrong operator', () => {
+            return assert.throws(() => parser(oneWrongOperator), /invalid/)
+        });
+
+        it('should rejects if have one correct operator', () => {
+            return assert.throws(() => parser(oneCorrectOperator), /invalid/)
+        });
     })
 })
+
 
