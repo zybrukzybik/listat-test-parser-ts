d4c9fecc66845f3895b3d066fb899c43144ef546
OZ <zybrukzybik997@gmail.com>
2021-03-01 20:19:12 +0200

    add isHaveExpressionsArr function

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -10,6 +10,10 @@ function isObj(obj: Object): Boolean {
     return ((obj === null) || (Array.isArray(obj)) || typeof obj == 'function') ? false : (typeof obj == 'object')
 }
 
+function isHaveExpressionsArr(obj): Boolean {
+    return (Object.keys(obj)[0] === 'expressions')
+}
+
 function isEmptyObj(obj: Object): Boolean {
     return (Object.keys(obj).length === 0)
 }
@@ -24,5 +28,6 @@ export {
     isEmptyArr,
     isObj,
     isEmptyObj,
+    isHaveExpressionsArr,
     isArrWithOneEmptyEl
 }
